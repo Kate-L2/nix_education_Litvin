2cc2ba4d6d514f229d09e2226f8bc2e4ce1b5bf8
Kate <zlatovlaskaya27@gmail.com>
2022-08-03 21:44:48 +0200

    1 and 2 tasks

--- hw-3.js
+++ hw-3.js
@@ -1,4 +1,4 @@
-import { emplyeeArr } from "testArray.js";
+import { emplyeeArr } from "./testArray.js";
 
 class Employee {
   constructor(employee) {
@@ -10,15 +10,40 @@ class Employee {
     this.isPrivileges = employee.isPrivileges;
     this.gender = employee.gender;
   }
-  getFullName() {
-    return this.surname + " " + this.name;
-  }
 }
 
-emplyeeArr.map((el) => {
-  let obj = new Employee(el);
-  console.log(obj);
-});
+// Task 1
+function Task1() {
+  emplyeeArr.map((el) => {
+    let obj = new Employee(el);
+    console.log(obj);
+  });
+}
+Task1();
+console.log("--------------------------------------");
+// Task 2
+function Task2() {
+  Employee.prototype.getFullName = function () {
+    return this.surname + " " + this.name;
+  };
+  emplyeeArr.map((el) => {
+    let obj = new Employee(el);
+    console.log(obj.getFullName());
+  });
+}
+Task2();
+console.log("--------------------------------------");
 
-// const employeeObj = new Employee(obj);
-// console.log(employeeObj.getFullName());
+function Task3() {
+  let obj;
+  let createEmployesFromArr = (array) => {
+    array.map((item) => {
+      obj.push(new Employee(item));
+    });
+
+    return employeeObjArr;
+  };
+  const emplyeeConstructArr = createEmployesFromArr(emplyeeArr);
+  console.log(emplyeeConstructArr);
+}
+Task3();
