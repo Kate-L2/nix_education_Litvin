fa415d3738d06c41f3f0e066455b780370161876
Kate <zlatovlaskaya27@gmail.com>
2022-08-11 14:25:35 +0200

    Done with 3, 4 tasks

--- hw-5.js
+++ hw-5.js
@@ -43,21 +43,44 @@ func1([999]);
 func1("4he6llo");
 
 // Task 3
-// const getTime = () => {
-//   let start = new Date();
-//   let startSec = start.getSeconds();
-//   console.log("Enabled");
-//   return function () {
-//     let end = new Date();
-//     let endSec = end.getSeconds();
-//     console.log(endSec - startSec);
-//   };
-// };
+const getTime = () => {
+  let create = false;
+  let start = new Date().getTime();
+  return function () {
+    if (!create) {
+      create = true;
+      console.log("Enabled");
+    }
+    let end = new Date().getTime();
+    console.log((end - start) / 1000);
+    start = new Date().getTime();
+  };
+};
 
-// let func2 = getTime();
-// func2();
+let func2 = getTime();
+func2();
+setTimeout(func2, 3000);
+setTimeout(func2, 6000);
 
 
-// let start = new Date();
-// let startSec = start.getSeconds();
-// console.log(startSec);
+// Task 4
+function clock() {
+  myTimer = setInterval(myClock, 1000);
+  let time = 20; //Initially set to 1 hour
+
+  function myClock() {
+    --time;
+    let seconds = time % 60; // Seconds that cannot be written in minutes
+    let secondsInMinutes = (time - seconds) / 60; // Gives the seconds that COULD be given in minutes
+    let minutes = secondsInMinutes % 60; // Minutes that cannot be written in hours
+    let hours = (secondsInMinutes - minutes) / 60;
+    console.clear();
+    console.log(hours + ":" + minutes + ":" + seconds);
+    if (time == 0) {
+      clearInterval(myTimer);
+      console.log("Time End");
+    }
+  }
+}
+
+// clock();
