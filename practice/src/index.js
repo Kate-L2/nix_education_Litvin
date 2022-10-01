377ea0fe8b2a22d8764059a05bee7ee6fdc8cb16
Kate <zlatovlaskaya27@gmail.com>
2022-10-01 20:27:59 +0200

    Node start

---
+++ practice/src/index.js
@@ -0,0 +1,12 @@
+// import readline from "readline";
+
+console.log("Hello");
+
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+readline.question("Who are you?", (name) => {
+  console.log(`Hey there ${name}!`);
+  readline.close();
+});
