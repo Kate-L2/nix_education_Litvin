38692dccc10ac51e0366064eb2cf112b7931eebc
Kate <zlatovlaskaya27@gmail.com>
2022-10-06 16:34:43 +0200

    Added second part of hw-1

--- practice/src/calc.js
+++ practice/src/calc.js
@@ -1,4 +1,7 @@
-const calc = require("./index.js");
+// const calc = require("./index.js");
 
-let sum = calc.square(3) + calc.cube(3);
-console.log(sum);
+// let sum = calc.square(3) + calc.cube(3);
+// console.log(sum);
+
+// console.log(__dirname);
+// console.log(__filename);
--- practice/src/index.js
+++ practice/src/index.js
@@ -1,24 +1,115 @@
-module.exports.square = function(num) {
-	return num ** 2;
+import * as http from "http";
+import { readFile } from "node:fs";
+
+const host = "localhost";
+const port = 8000;
+
+let indexFile;
+let body;
+
+const users = [];
+
+const requestListener = function (req, res) {
+  try {
+    switch (req.url) {
+      case "/users":
+        userController(req, res);
+        break;
+      case "/users/createWithArray":
+        userController(req, res);
+        break;
+      case "/users/:userName":
+        userController(req, res);
+        break;
+      case "/":
+        res.end(indexFile);
+        break;
+      default:
+        res.setHeader("Content-Type", "application/json");
+        res.writeHead(404);
+        res.end(`{code: 404, message: "Resource not found"}`);
+    }
+  } catch (e) {
+    res.setHeader("Content-Type", "application/json");
+    res.writeHead(500);
+    res.end(JSON.stringify(e));
+  }
+};
+
+function userController(req, res) {
+  switch (req.method) {
+    case "POST":
+      postFunc(req, (body) => {
+        users.push(body);
+      });
+      res.setHeader("Content-Type", "application/json");
+      res.writeHead(200);
+      res.end("Message: saved");
+      break;
+    case "GET":
+      res.setHeader("Content-Type", "application/json");
+      res.writeHead(200);
+      res.end(JSON.stringify(users));
+      break;
+    case "PUT":
+      res.setHeader("Content-Type", "application/json");
+      res.writeHead(200);
+      res.end(JSON.stringify(users));
+      break;
+    case "DELETE":
+      if (deleteFunc(req)) {
+        res.writeHead(200);
+        res.end("User was deleted");
+      } else {
+        res.writeHead(400);
+        res.end("There is no user with such name");
+      }
+
+      break;
+    default:
+      res.setHeader("Content-Type", "application/json");
+      res.writeHead(200);
+      res.end("Error");
+      break;
+  }
 }
 
-module.exports.cube = function (num) {
-	return num ** 3;
+function postFunc(req, func) {
+  body = [];
+  req
+    .on("data", (chunk) => {
+      body.push(chunk);
+    })
+    .on("end", () => {
+      body = Buffer.concat(body).toString();
+      console.log(body);
+      func(body);
+    });
 }
 
-module.exports.avg = function (arr) {
-	return sum(arr, 1) / arr.length;
+function deleteFunc(req) {
+  const { name } = req.param;
+  const deleted = users.find((user) => user.name === name);
+
+  if (deleted) {
+    users = users.filter((el) => el.name !== param);
+    return users;
+  } else {
+    return false;
+  }
 }
 
-module.exports.digitsSum = function(num) {
-	return sum(String(num).split(''));
-}
+const server = http.createServer(requestListener);
 
-// допоміжних функція
-function sum(arr) {
-	let res = 0;
-	for (let elem of arr) {
-		res += +elem;
-	}
-	return res;
-}
+readFile(process.cwd() + "/src/resource/index.html", "utf8", (err, data) => {
+  if (err) {
+    console.error(`Could not read index.html file: ${err}`);
+    process.exit(1);
+    return;
+  }
+  indexFile = data;
+  server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+  });
+});
+
