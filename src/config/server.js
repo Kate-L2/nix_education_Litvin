d66c769bfe6725e9c913e4dae359638cfb7004b7
Kate <zlatovlaskaya27@gmail.com>
2022-11-23 13:32:29 +0100

    finish

--- src/config/server.js
+++ src/config/server.js
@@ -8,6 +8,12 @@ connectionDB();
 var port = process.env.PORT || 8080;
 
 app.use("/", router);
+app.get("*", function (req, res) {
+  res.redirect("https://mac-outlet-store.herokuapp.com/login");
+
+  // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
+  // res.redirect('https://example.com' + req.url);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
